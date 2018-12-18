16e0b54563db0a2a43c6af51de49574cb10ca65c
Tim Woods <jack3760@aol.com>
2018-12-18 04:35:18 +0000

    登录成功后导航逻辑

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css').version();
+/*
    .sass('resources/sass/app.scss', 'public/css', {
         outputStyle: 'expanded'
    }).version();
+*/
