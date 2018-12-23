97886e5b70a73c3f6ccd802895cab0c240372b95
Tim Woods <jack3760@aol.com>
2018-12-23 14:01:16 +0000

    密码重置

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,71 +7,103 @@ use App\Models\User;
 use Auth;
 use Mail;
 
+/**
+ * Class UsersController
+ * @package App\Http\Controllers
+ */
 class UsersController extends Controller
 {
+    /**
+     * UsersController constructor.
+     */
     public function __construct()
     {
-
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail'],
         ]);
 
         $this->middleware('guest', [
-            'only' => ['create']
+            'only' => ['create'],
         ]);
     }
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $users = User::paginate(10);
+
         return view('users.index', compact('users'));
     }
-    //
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view('users.create');
     }
 
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
+            'password' => 'required|confirmed|min:6',
         ]);
-
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->password)
+            'password' => bcrypt($request->password),
         ]);
 
         $this->sendEmailConfirmationTo($user);
         session()->flash('success', '账户注册成功，验证邮件已发送至您的邮箱，请查收');
+
         return redirect('/');
-        //Auth::login($user);
-        //session()->flash('success', '欢迎，您将在这里开启一段新的旅程!');
-        //return redirect()->route('users.show', [$user]);
     }
 
+    /**
+     * @param  User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function edit(User $user)
     {
         $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
+    /**
+     * @param User $user
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function update(User $user, Request $request)
     {
         $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
-            'password' => 'nullable|confirmed|min:6'
+            'password' => 'nullable|confirmed|min:6',
         ]);
 
         $data = [];
@@ -83,17 +115,27 @@ class UsersController extends Controller
         $user->update($data);
 
         session()->flash('success', '您的资料更新成功');
+
         return redirect()->route('users.show', $user->id);
     }
 
+    /**
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function destroy(User $user)
     {
         $this->authorize('destroy', $user);
         $user->delete();
-        session()->flash('success', '用户' . $user->name . '删除成功');
+        session()->flash('success', '用户'.$user->name.'删除成功');
+
         return back();
     }
 
+    /**
+     * @param $user
+     */
     public function sendEmailConfirmationTo($user)
     {
         $view = 'emails.confirm';
@@ -101,25 +143,29 @@ class UsersController extends Controller
         $from = 'summer@example.com';
         $name = 'Summer';
         $to = $user->email;
-        $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+        $subject = '感谢注册 Weibo 应用！请确认你的邮箱。';
 
         Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
             $message->from($from, $name)->to($to)->subject($subject);
         });
     }
 
-    public  function confirmEmail($token)
+    /**
+     * @param $token
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function confirmEmail($token)
     {
         $user = User::where('activation_token', $token)->firstOrFail();
 
         $user->activated = true;
         $user->activation_token = null;
-        $user->email_verified_at = Date('Y-m-d H:i:s');
+        $user->email_verified_at = date('Y-m-d H:i:s');
         $user->save();
 
         Auth::login($user);
         session()->flash('success', '账户激活成功，恭喜您');
-        return redirect()->route('users.show', $user);
 
+        return redirect()->route('users.show', $user);
     }
 }
